fc01a79d9430abf6653969d604e3cd1cdad2579c
Kamski18 <addinzafran1505@gmail.com>
2026-01-03 23:54:46 +0800

    Careless mistake

--- plugins/prayer_time.py
+++ plugins/prayer_time.py
@@ -5,13 +5,12 @@ from datetime import date
 
 async def remind(message):
     coordinate = (2.1627822, 102.3349452)
-    params = {"method" : 'ISNA'}
+    params = ISNA
     today = date.today()
-
-    times = adhan(day=today, location=coordinate, parameters=params)
 
     try:
         result = [f"Prayer time for **{today}**\n\n"]
+        times = adhan(day=today, location=coordinate, parameters=params)
 
         for name, time in times.items():
             formatted_time = strftime("%H : %M")
