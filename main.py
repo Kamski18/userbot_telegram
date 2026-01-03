06474f47a62820be7fe579427515db9c6ee4423d
Kamski18 <addinzafran1505@gmail.com>
2026-01-03 22:09:19 +0800

    Adding session string for hosting

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import logging
 from pyrogram import Client, filters # type: ignore
 from plugins import gemini
+import os
 
 # 3. Memory: Disable verbose logging if not needed, or use a leaner format
 logging.basicConfig(level=logging.WARNING) 
+SESSION_STRING = os.getenv("SESSION_STRING")
 
 app = Client(
     name="Kamski's Acount",
     api_hash=gemini.API_HASH, # type: ignore
     api_id=gemini.API_ID, # type: ignore
+    session_string=SESSION_STRING, # type:ignore
     sleep_threshold=10,      # Optimization: Don't wait too long on flood waits
     in_memory=True           # Memory: Keep session in RAM to avoid slow Disk I/O
 )
