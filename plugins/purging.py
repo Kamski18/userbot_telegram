3c5382dae3bc42e25bd722ca79bc21f349f961fc
Kamski18 <addinzafran1505@gmail.com>
2026-01-06 20:42:00 +0800

    careless mistake again

--- plugins/purging.py
+++ plugins/purging.py
@@ -5,7 +5,7 @@ async def purge(message):
         await message.edit_text("__Reply to a message!__")
         return
     
-    for i in range(message.reply_to.id, message.id, 100):
+    for i in range(message.reply_to_message.id, message.id, 100):
         await app.delete_messages(chat_id=message.chat.id, messsage_ids=list(range(i + 100, message.id))) # type: ignore
 
         await message.edit_text("__purge!, this message will auto delete in 5 seconds__")
