aa2e29ef5e7a743dfd2dfe61ed69ccde20abd68e
Kamski18 <addinzafran1505@gmail.com>
2026-01-06 20:49:13 +0800

    one s

--- plugins/purging.py
+++ plugins/purging.py
@@ -6,7 +6,7 @@ async def purge(client, message):
         return
     
     for i in range(message.reply_to_message.id, message.id, 100):
-        await client.delete_messages(chat_id=message.chat.id, messsage_ids=list(range(i + 100, message.id))) # type: ignore
+        await client.delete_messages(chat_id=message.chat.id, message_ids=list(range(i + 100, message.id))) # type: ignore
 
         await message.edit_text("__purge!, this message will auto delete in 5 seconds__")
         await asyncio.sleep(5)
