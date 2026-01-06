b164fcbd9568e8786f1f164f8a08bc7f5a7d8f04
Kamski18 <151996410+Kamski18@users.noreply.github.com>
2026-01-06 20:54:00 +0800

    Refactor purge function to improve message deletion

--- plugins/purging.py
+++ plugins/purging.py
@@ -4,10 +4,19 @@ async def purge(client, message):
     if not message.reply_to_message:
         await message.edit_text("__Reply to a message!__")
         return
-    
-    for i in range(message.reply_to_message.id, message.id, 100):
-        await client.delete_messages(chat_id=message.chat.id, message_ids=list(range(i + 100, message.id))) # type: ignore
 
-        await message.edit_text("__purge!, this message will auto delete in 5 seconds__")
-        await asyncio.sleep(5)
-        await message.delete()
+    start_id = message.reply_to_message.id
+    end_id = message.id
+
+    ids = list(range(start_id, end_id))
+
+    for i in range(0, len(ids), 100):
+        await client.delete_messages(
+            chat_id=message.chat.id,
+            message_ids=ids[i:i + 100]
+        )
+        await asyncio.sleep(0.3)  # prevent floodwait
+
+    status = await message.edit_text("__Purge completed. Auto deleting...__")
+    await asyncio.sleep(5)
+    await status.delete()
